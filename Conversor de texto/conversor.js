7c2578d92eea9d4d4c81cb4c7e0d55f9ac005d73
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-02-21 11:06:58 -0300

    Create conversor.js

---
+++ Conversor de texto/conversor.js
@@ -0,0 +1,13 @@
+
+   var t = document.getElementById('text')
+   var r = document.getElementById('resultado')
+
+
+   function maiuscula(){
+    r.innerText = t.value.toUpperCase()
+   }
+
+   function minuscula(){
+    r.innerText = t.value.toLowerCase()
+   }
+     
