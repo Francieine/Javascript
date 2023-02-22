60f649d0a213654ee09efc354d4d1957bd54c2d5
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-02-22 14:28:18 -0300

    Create exibeeoculta.js

---
+++ Ocultar & Exibir/exibeeoculta.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#exibir").click(function(){
+        $("#div02").show("slow");    
+   });
+
+    $("#ocultar").click(function(){
+        $("#div02").hide("slow");
+    });
+});
+
